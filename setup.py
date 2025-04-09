1c545c84168122ca9c558be92cf8dd6972f034bc
Falanya001 <152361339+Falanya001@users.noreply.github.com>
2025-04-09 10:45:25 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,28 +23,21 @@ setup(
     packages=find_packages(),
     package_data={'': ['*.yaml', '*.json']},    # This will include all yaml files in package
     install_requires=[
-        'python-dotenv',
-        'openai==1.37.0',
-        'httpx==0.27.2',
+        'python-dotenv', 
+        "openai==1.37.2", 
         'tiktoken',
-        'PyYAML>=6.0.1',  # For YAML file handling
         'tinydb',
-        'pymongoarrow>=1.0.0',  # Instead of tinymongo
+        'tinymongo',
         'proglog',
-        'yt-dlp>=2025.1.12',
+        'yt-dlp',
         'torch',
-        'torchaudio',
-        'srt==3.5.3',  # Required by whisper-timestamped
         'whisper-timestamped',
-        'protobuf==3.20.3',
-        'pillow>=9.2.0,<11.0',
+        'torchaudio',
+        'pillow==10.4.0',
+        'edge-tts',
         'moviepy==2.1.2',
-        'edge-tts==7.0.0',
-        'tqdm>=4.65.0',  # Instead of progress
+        'progress',
         'questionary',
-        'fsspec[http]<=2024.12.0,>=2023.1.0',
-        'numpy<2.2,>=1.24',
-        'datasets>=3.5.0',
     ],
     keywords=['python', 'video', 'content creation', 'AI', 'automation', 'editing', 'voiceover synthesis', 'video captions', 'asset sourcing', 'tinyDB'],
     classifiers=[
